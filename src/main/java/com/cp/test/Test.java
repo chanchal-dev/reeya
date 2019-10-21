e9b2eba1994bb2524bcecee0b0f7f4c45bd24ffc
chanchal <chanchalprajapat86@gmail.com>
2019-10-21 11:59:36 +0530

    added statement

--- src/main/java/com/cp/test/Test.java
+++ src/main/java/com/cp/test/Test.java
@@ -5,6 +5,9 @@ public class Test {
    public static void main(String[] args) {
 	   
 	   System.out.println("test");
+	   System.out.println("test2");
+	   System.out.println("test3");
+	   
 	
 }
 		
